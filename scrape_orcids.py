ae8a89f1fef52a3e7aea3d23a85f0c80ea9dd40b
Hubert Plisiecki <hplisiecki@gmail.com>
2022-11-15 17:31:26 +0100

    update

--- scrape_orcids.py
+++ scrape_orcids.py
@@ -1,144 +1,10 @@
-import pandas as pd
-from tqdm import tqdm
-import time
-import re
-
-# re find date format in string
-def find_date(string):
-    pattern = re.compile(r'\d{4}-\d{2}-\d{2}')
-    date = pattern.findall(string)
-    # if date is not found
-    if len(date) == 0:
-        # try format year month
-        pattern = re.compile(r'\d{4}-\d{2}')
-        date = pattern.findall(string)
-    if len(date) == 0:
-        # try format year
-        pattern = re.compile(r'\d{4}')
-        date = pattern.findall(string)
-    if len(date) == 0:
-        date = ['unrecovered']
-    return date
-
-
-# wos = 'https://www.webofscience.com/wos/author/record/2408412'
-# driver.get(wos)
-# time.sleep(4)
-# html = driver.page_source
-# soup = bs(html, 'html.parser')
-
-swps_links = pd.read_csv('data/swps_links.csv')
-
-checked = swps_links[swps_links['Checked'] == True]
-
-name_orcid = [(fullname, orcid) for fullname, orcid in zip(swps_links.fullname, swps_links.orcid) if 'orcid' in str(orcid)]
-
-
-# scrape orcid website
-from selenium import webdriver
-from bs4 import BeautifulSoup as bs
-# geckodriver
-options = webdriver.FirefoxOptions()
-
-options.add_argument('--no-sandbox')
-options.add_argument('user-agent=Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0')
-driver = webdriver.Firefox(executable_path='D:/PycharmProjects/webdriver/geckodriver.exe' , options=options)
-
-# swps_publication_dir = r'D:\data\ranking\swps'
-
-date_range = (2017, 2022)
-
-df = pd.DataFrame()
-links_list = []
-names_list = []
-orcids_list = []
-failed = []
-dates_list = []
-too_long = []
-for name, orcid in tqdm(name_orcid):
-    try:
-        driver.get(orcid)
-        time.sleep(3)
-        html = driver.page_source
-        soup = bs(html, 'html.parser')
-        # find strong
-        works = soup.find_all('div', {'class': 'orc-font-body-large clickable'})
-        works = [w for w in works if 'Works' in w.text][0].text
-
-
-        # get all divs with class 'panel-data-container'
-        divs = soup.find_all('div', {'class': 'panel-data-container'})
-        # get all divs containing 'Journal article
-        divs = [div for div in divs if 'Journal article' in str(div)]
-        # get all in every div with class 'general-data' and not class 'general-data ng-star-inserted'
-        d_divs = [div.find_all('div', {'class': 'general-data'}) for div in divs]
-        cleaned = []
-        for d_div in d_divs:
-            for d in d_div:
-                if 'general-data ng-star-inserted' not in str(d):
-                    cleaned.append(d)
-        dates = [find_date(str(div))[0] for div in cleaned]
-        # check if there is a date lower than 2017
-        if 'of' in works:
-            if not any([date < str(date_range[0]) for date in dates]):
-                too_long.append(orcid)
-                continue
-        divs_in_range = []
-        limited_dates = []
-        for (div, date) in zip(divs, dates):
-            # check if date is within range
-            if date == 'unrecovered':
-                divs_in_range.append(div)
-                limited_dates.append(date)
-            elif int(date.split('-')[0]) in range(date_range[0], date_range[1]):
-                divs_in_range.append(div)
-                limited_dates.append(date)
-
-        # find http links in every div
-        links = []
-        for div in divs_in_range:
-            a = div.find_all('a')
-            link = 'empty'
-            for i in a:
-                if 'http' in str(i) and 'doi' in str(i):
-                    # extract link
-                    link = str(i).split('href="')[1].split('"')[0]
-
-            if link == 'empty':
-                title_container = div.parent.parent.parent.previousSibling
-                # get h2
-                link = title_container.find('h2').text # get title
-
-            links.append(link)
-
-        links_list.extend(links)
-        names_list.extend([name] * len(links))
-        orcids_list.extend([orcid] * len(links))
-        dates_list.extend(limited_dates)
-
-    except:
-        failed.append(orcid)
-
-# save a = div.parent.parent.parent.previousSibling
-df = pd.DataFrame({'name': names_list, 'orcid': orcids_list, 'link': links_list, 'date': dates_list})
-
-
-df.to_csv('data/swps_publication_links.csv', index=False)
-
-publications = pd.read_csv('data/swps_publication_links.csv')
-# import matplotlib
-# matplotlib.use('TkAgg')
-# import matplotlib.pyplot as plt
-# counts = publications.name.value_counts()
-# # plot
-# plt.figure(figsize=(10, 10))
-# plt.barh(counts.index, counts.values)
-
-def redo_on_one(orcid, name):
-    driver.get(orcid)
-    time.sleep(3)
-    html = driver.page_source
+def get_doi_from_html(html):
+    longer = False
     soup = bs(html, 'html.parser')
+    # find strong
+    works = soup.find_all('div', {'class': 'orc-font-body-large clickable'})
+    works = [w for w in works if 'Works' in w.text][0].text
+
     # get all divs with class 'panel-data-container'
     divs = soup.find_all('div', {'class': 'panel-data-container'})
     # get all divs containing 'Journal article
@@ -151,6 +17,11 @@ def redo_on_one(orcid, name):
             if 'general-data ng-star-inserted' not in str(d):
                 cleaned.append(d)
     dates = [find_date(str(div))[0] for div in cleaned]
+    # check if there is a date lower than 2017
+    if 'of' in works:
+        if not any([date < str(date_range[0]) for date in dates]):
+            longer = True
+
     divs_in_range = []
     limited_dates = []
     for (div, date) in zip(divs, dates):
@@ -179,14 +50,97 @@ def redo_on_one(orcid, name):
 
         links.append(link)
 
-    links_list.extend(links)
-    names_list.extend([name] * len(links))
-    orcids_list.extend([orcid] * len(links))
-    dates_list.extend(limited_dates)
+    return links, longer
 
-# soup beautify html
-# soup = bs(html, 'html.parser')
-# # get all http links
-# links = [link.get('href') for link in soup.find_all('a') if 'http' in str(link.get('href'))]
-# links  [link for link in links if 'doi' in link]
+def find_date(string):
+    pattern = re.compile(r'\d{4}-\d{2}-\d{2}')
+    date = pattern.findall(string)
+    # if date is not found
+    if len(date) == 0:
+        # try format year month
+        pattern = re.compile(r'\d{4}-\d{2}')
+        date = pattern.findall(string)
+    if len(date) == 0:
+        # try format year
+        pattern = re.compile(r'\d{4}')
+        date = pattern.findall(string)
+    if len(date) == 0:
+        date = ['unrecovered']
+    return date
+
+def scrape_orcids(fullnames, orcids, save_path):
+    name_orcid = [(fullname, orcid) for fullname, orcid in zip(fullnames, orcids) if 'orcid' in str(orcid)]
+
+    # scrape orcid website
+    from selenium import webdriver
+    from bs4 import BeautifulSoup as bs
+    # geckodriver
+    options = webdriver.FirefoxOptions()
+
+    options.add_argument('--no-sandbox')
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0')
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+    # swps_publication_dir = r'D:\data\ranking\swps'
+
+    date_range = (2017, 2022)
+
+    links_list = []
+    names_list = []
+    orcids_list = []
+    failed = []
+    dates_list = []
+    for name, orcid in tqdm(name_orcid):
+        try:
+            driver.get(orcid)
+            time.sleep(5)
+            html = driver.page_source
+            links, longer = get_doi_from_html(html)
+
+            if longer:
+                try:
+                    button_css_path = 'html body app-root.desktop.columns-12 div div.router-container app-my-orcid.ng-star-inserted div.container.no-padding.ng-star-inserted div.row main#main.col.l9.m8.s4 app-main app-work-stack-group#cy-works.ng-star-inserted form#cy-works-form.ng-untouched.ng-pristine.ng-valid.ng-star-inserted app-panels#cy-works-panels.row.ng-star-inserted div.col.s4.m8.l12.content.no-gutters.ng-star-inserted mat-paginator.mat-paginator.ng-star-inserted div.mat-paginator-outer-container div.mat-paginator-container div.mat-paginator-range-actions button.mat-focus-indicator.mat-tooltip-trigger.mat-paginator-navigation-next.mat-icon-button.mat-button-base span.mat-button-wrapper svg.mat-paginator-icon'
+                    button = driver.find_element('css selector', button_css_path)
+                    button.click()
+                    time.sleep(5)
+                    html = driver.page_source
+                    more_links, longer = get_doi_from_html(html)
+                    links.extend(more_links)
+
+                except:
+                    pass
+
+            links_list.extend(links)
+            names_list.extend([name] * len(links))
+            orcids_list.extend([orcid] * len(links))
+            dates_list.extend(limited_dates)
+
+        except:
+            failed.append(orcid)
+
+    # save a = div.parent.parent.parent.previousSibling
+    df = pd.DataFrame({'name': names_list, 'orcid': orcids_list, 'link': links_list, 'date': dates_list})
+
+    df.to_csv(save_path, index=False)
+
+    return failed
+
+
+def main():
+    import pandas as pd
+    from tqdm import tqdm
+    import time
+    import re
+    from selenium.webdriver.firefox.service import Service as FirefoxService
+    from webdriver_manager.firefox import GeckoDriverManager
+    import pandas as pd
+    swps_links = pd.read_csv('data/swps_links.csv')
+    checked = swps_links[swps_links['Checked'] == True]
+    failed = scrape_orcids(checked['fullname'].values, checked['orcid'].values, 'data/swps_publication_links.csv')
+    print('failed', failed)
+    # publications = pd.read_csv('data/swps_publication_links.csv')
+
+if __name__ == '__main__':
+    # this code is best ran manually in the console because of the need to input some people manually
+    main()
 
--- scrape_pdf.py
+++ scrape_pdf.py
@@ -1,123 +1,127 @@
-from tqdm import tqdm
-import pandas as pd
-
-# downloading and merging files ##################################################
-publications = pd.read_csv('data/swps_publication_links.csv')
-
-wos = pd.read_csv('data/swps_wos_links.csv')
-# drop nans from links
-wos = wos.dropna(subset=['doi'])
-wos['name'] = wos['fullname']
-wos['link'] = ['https://doi.org/' + l for l in wos['doi']]
-del wos['fullname']
-del wos['doi']
-
-# merge
-publications = pd.concat([publications, wos], ignore_index=True)
-publications = publications.drop_duplicates(subset=['link'])
-# reset index
-publications = publications.reset_index(drop=True)
-# save
-publications.to_csv('data/swps_publication_merged.csv', index=False)
-##################################################################################
 
 
-# adding information about titles and ids ########################################
-publications = pd.read_csv('data/swps_publications_merged.csv')
-ids = []
-for name in publications.name.unique():
-    df_name = publications[publications.name == name]
-    for i in range(len(df_name)):
-        ids.append(name.replace(' ', '_') + '_' + str(i + 1))
+def merge_and_make_ids(institute):
+    orcid_df = pd.read_csv(f'data/swps_publication_links.csv')
+    wos_df = pd.read_csv('data/swps_wos_links.csv')
 
-publications['id'] = ids
+    # drop nans from links
+    wos_df = wos_df.dropna(subset=['doi'])
+    wos_df['name'] = wos_df['fullname']
+    wos_df['link'] = ['https://doi.org/' + l for l in wos_df['doi']]
+    del wos_df['fullname']
+    del wos_df['doi']
 
-links = [1 if 'https' in p else 0 for p in publications.link.values ]
-publications['is_a_link'] = links
-publications.to_csv('data/swps_publications_merged_ids.csv', index=False)
-##################################################################################
+    # merge
+    publications = pd.concat([orcid_df, wos_df], ignore_index=True)
+    publications = publications.drop_duplicates(subset=['link'])
+    # reset index
+    publications = publications.reset_index(drop=True)
 
-# load
-# automatic filling of the links, not used for some reason #######################
-publications = pd.read_csv('data/swps_publications_merged_ids.csv')
+    ids = []
+    for name in publications.name.unique():
+        df_name = publications[publications.name == name]
+        for i in range(len(df_name)):
+            ids.append(name.replace(' ', '_') + '_' + str(i + 1))
 
-titles = publications[publications.is_a_link == 0].link.values
+    publications['id'] = ids
 
-titles_df = publications[publications.is_a_link == 0]
+    links = [1 if 'https' in p else 0 for p in publications.link.values ]
+    publications['is_a_link'] = links
+    publications.to_csv(f'data/{institute}_publications_merged_ids.csv', index=False)
 
-import mechanize
-browser = mechanize.Browser()
-browser.set_handle_robots(False)
-browser.addheaders = [('User-agent', 'Firefox')]
 
-links = []
-for number in tqdm(range(len(titles))):
-    title = titles[number]
-    proper = False
-    while proper == False:
+
+def automatic_title_fill(institute):
+    publications_df = pd.read_csv(f'data/{institute}_publications_merged_ids.csv')
+
+
+    titles = publications_df[publications_df.is_a_link == 0].link.values
+
+    titles_df = publications_df[publications_df.is_a_link == 0]
+
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    browser.addheaders = [('User-agent', 'Firefox')]
+
+    links = []
+    for number in tqdm(range(len(titles))):
+        title = titles[number]
+        proper = False
+        while proper == False:
+            try:
+                browser.open('https://sci-hub.se')
+                browser.select_form(nr=0)
+                proper = True
+            except:
+                print('sleep')
+                time.sleep(60)
+        # use form
+        browser['request'] = title
+        response = browser.submit()
+        links.append(browser.geturl())
+        print(browser.geturl())
+
+    cnt = 0
+    for idx, link in enumerate(links):
+        if link != 'https://sci-hub.se/':
+            cnt += 1
+            # change the value in the dataframe
+            titles_df.iloc[idx, 2] = link
+            # get the index of the row
+            index = titles_df[titles_df['link'] == link].index[0]
+            # change the value in the dataframe
+            publications_df.iloc[index, 2] = link
+
+    publications_df.to_csv(f'data/{institute}_publications_merged_ids_after_auto_title.csv')
+
+    titles_df = publications[publications.is_a_link == 0].reset_index(drop=True)
+    titles_df.to_csv(f'data/{institute}_titles.csv', index=False)
+
+
+
+def download_pdfs(institute):
+    save_path = rf'D:\data\ranking\{institute}/'
+
+    publications_df = pd.read_csv(f'data/{institute}_publications_merged_ids_after_auto_title.csv')
+    publications_with_links = publications_df[publications_df.is_a_link == 1].reset_index(drop=True)
+    sh = SciHub()
+    failed = []
+    automatic_pdf_download = []
+    for link, id in tqdm(zip(publications_with_links.link.values, publications_with_links.id.values)):
         try:
-            browser.open('https://sci-hub.se')
-            browser.select_form(nr=0)
-            proper = True
+            name = str(id) + '.pdf'
+            result = sh.download(link, path = name , destination= save_path)
+            automatic_pdf_download.append(1)
         except:
-            print('sleep')
-            time.sleep(60)
-    # use form
-    browser['request'] = title
-    response = browser.submit()
-    links.append(browser.geturl())
-    print(browser.geturl())
+            failed.append(link)
+            automatic_pdf_download.append(0)
 
-cnt = 0
-for idx, link in enumerate(links):
-    if link != 'https://sci-hub.se/':
-        cnt += 1
-        # change the value in the dataframe
-        titles_df.iloc[idx, 2] = link
-        # get the index of the row
-        index = titles_df[titles_df['link'] == link].index[0]
-        # change the value in the dataframe
-        publications.iloc[index, 2] = link
+    import os
+    files = os.listdir(save_path)
+    files = [f.replace('.pdf', '') for f in files if '.pdf' in f]
+    downloaded = [1 if i in files else 0 for i in publications_with_links.id.values]
+    publications_with_links['automatic_pdf_download'] = downloaded
+    # save
+    publications_with_links.to_csv(f'data/{institute}_publications_with_links_after_auto_pdf.csv', index=False)
 
-publications.to_csv('data/swps_publications_merged_ids.csv', index=False)
 
-titles_df = publications[publications.is_a_link == 0].reset_index(drop=True)
+def main():
+    from scihub_api import SciHub
+    from tqdm import tqdm
+    import mechanize
+    from tqdm import tqdm
+    import pandas as pd
+    institute = 'swps'
 
-# save
-titles_df.to_csv('data/swps_titles.csv', index=False)
+    merge_and_make_ids(institute)
+    automatic_title_fill(institute)
+    download_pdfs(institute)
 
-# load
-titles_df = pd.read_csv('data/swps_titles.csv')
+if __name__ == '__main__':
+    main()
 
-# Downloading the pdfs ###########################################################
-from scihub_api import SciHub
-import pandas as pd
-from tqdm import tqdm
-save_path = r'D:\data\ranking\swps/'
 
-# load
-publications = pd.read_csv('data/swps_publications_merged_ids.csv')
 
-publications_with_links = publications[publications.is_a_link == 1].reset_index(drop=True)
-sh = SciHub()
-failed = []
-automatic_pdf_download = []
-for link, id in tqdm(zip(publications_with_links.link.values, publications_with_links.id.values)):
-    try:
-        name = str(id) + '.pdf'
-        result = sh.download(link, path = name , destination= save_path)
-        automatic_pdf_download.append(1)
-    except:
-        failed.append(link)
-        automatic_pdf_download.append(0)
-
-import os
-files = os.listdir(save_path)
-files = [f.replace('.pdf', '') for f in files if '.pdf' in f]
-downloaded = [1 if i in files else 0 for i in publications_with_links.id.values]
-publications_with_links['automatic_pdf_download'] = downloaded
-# save
-publications_with_links.to_csv('data/swps_publications_with_links_after_auto_pdf.csv', index=False)
 ##################################################################################
 # get list of files in the save_path
 # load
@@ -165,25 +169,6 @@ new_url = driver.current_url
 'https://doi.org/10.15678/pjoep.2017.11.03'
 'https://doi.org/10.15678/pjoep.2017.11.04'
 'https://doi.org/10.7420/ak2018a'
-# import requests
-#
-# failed = []
-# for idx, link in tqdm(enumerate(links)):
-#     try:
-#         path = f'https://sci-hub.se/{link}'
-#         driver.get(path)
-#         # download pdf
-#         time.sleep(2)
-#         html = driver.page_source
-#         soup = bs(html, 'html.parser')
-#         pdf = soup.find('embed', {'id': 'pdf'})
-#         pdf_link = 'https:' + pdf['src']
-#         # save pdf
-#         pdf = requests.get(pdf_link)
-#         with open(save_path + f'{idx}.pdf', 'wb') as f:
-#             f.write(pdf.content)
-#     except:
-#         failed.append(idx)
 
 
 
