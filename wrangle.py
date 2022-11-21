cc7b471296c2cfe75dc265b44a7dbaecfc226f29
Paweł Lenartowicz <113543670+pawlenartowicz@users.noreply.github.com>
2022-11-21 08:46:57 +0100

    # comments

--- wrangle.py
+++ wrangle.py
@@ -9,7 +9,7 @@ import os
 # Registration i obligatory for almost all active scienists, especialy those who works at Universities
 df = pd.read_csv('./data/scientists.csv')
 
-#Selecting columns & changing names to English
+# Selecting columns & changing names to English
 df = df[['Id', 'Dane podstawowe - Imię', 'Dane podstawowe - Drugie imię',
          'Dane podstawowe - Przedrostek nazwiska', 'Dane podstawowe - Nazwisko', 
          'Zatrudnienie - Nazwa','Zatrudnienie - Podstawowe miejsce pracy',]]
@@ -17,7 +17,7 @@ df = df[['Id', 'Dane podstawowe - Imię', 'Dane podstawowe - Drugie imię',
 df.columns = ['id', 'name', 'second_name', 'pre_surname', 'surname', 
                 'uni_name', 'is_a_main_job']
 
-#filling missing names values
+# Filling missing names values
 prev_id = 'melon'
 names = []
 for index, row in df.iterrows():
@@ -39,24 +39,24 @@ for index, row in df.iterrows():
     names.append(' '.join(names_pieces))
 df['fullname'] = names
 
-#splitting into those, who declared "main job" and others
+# Splitting into those, who declared "main job" and others
 temp_df=df[df['is_a_main_job']=="Tak"]
 unlisted=df[~df['id'].isin(temp_df.id)]
 unlisted[['fullname', 'uni_name', 'is_a_main_job']].to_csv('./data/other/unlisted.csv')
 df=temp_df
 
-#selecting institutes with at least 25 listed scientists
+# Selecting institutes with at least 25 listed scientists
 institutes = df['uni_name'].value_counts()
 institutes = institutes[institutes > 24]
 institutes.to_csv('./data/institutes.csv')
 
-#selecting scientists who are connected to selected institutions
+# Selecting scientists who are connected to selected institutions
 df_selected=df[df['uni_name'].isin(institutes.index)]
 df_notselected=df[~df['id'].isin(df_selected.id)]
 df_notselected[['fullname','uni_name']].to_csv('./data/other/names.csv')
 
 selected=pd.DataFrame()
-#making files for selected scientists
+# Making files for selected scientists
 for i in institutes.index:
     x=df_selected[df_selected.uni_name==i]
     selected=pd.concat([selected,x[['fullname','uni_name']]])
@@ -65,11 +65,10 @@ for i in institutes.index:
         os.makedirs(dir)
     x[['fullname']].to_csv('%s/names.csv' % dir)
 
-#making Where is Wally
+# Making Where is Wally
 selected['file']='selected'
 df_notselected['file']='other-names'    
 unlisted['file']="other-unlisted"
-unlisted=unlisted.dropna()
 
 WhereIsWally = pd.concat([
     selected,
