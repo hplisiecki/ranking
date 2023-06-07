0764baf985b3edb0d67c880543424bf4fc7f2116
Hubert Plisiecki <hplisiecki@gmail.com>
2023-06-07 22:46:19 +0200

    internet archive scraper and a inspect failed function

--- inspect_failed_articles.py
+++ inspect_failed_articles.py
@@ -2,7 +2,19 @@ import pandas as pd
 import os
 
 dir = r'D:\data\ranking\pdfs\failed_pdf'
+save_path = r'D:\data\ranking\pdfs'
 
+
+unis = ['SWPS_Uniwersytet_Humanistycznospołeczny_z_siedzibą_w_Warszawie.csv', 'Akademia_Ignatianum_w_Krakowie.csv', 'Instytut_Psychologii_Polskiej_Akademii_Nauk.csv',
+          'Akademia_Pedagogiki_Specjalnej_Marii_Grzegorzewskiej_w_Warszawie.csv', 'Uniwersytet_Adama_Mickiewicza_w_Poznaniu.csv', 'Katolicki_Uniwersytet_Lubelski_Jana_Pawła_II_w_Lublinie.csv',
+          'Uniwersytet_Gdański.csv', 'Uniwersytet_Jagielloński_w_Krakowie.csv', 'Uniwersytet_Jana_Kochanowskiego_w_Kielcach.csv', 'Uniwersytet_Kardynała_Stefana_Wyszyńskiego_w_Warszawie.csv',
+          'Uniwersytet_Kazimierza_Wielkiego_w_Bydgoszczy.csv', 'Uniwersytet_Marii_Curie-Skłodowskiej_w_Lublinie.csv', 'Uniwersytet_Pedagogiczny_Komisji_Edukacji_Narodowej_w_Krakowie.csv',
+          'Uniwersytet_Szczeciński.csv', 'Uniwersytet_Warszawski.csv', 'Uniwersytet_Wrocławski.csv', 'Uniwersytet_Łódzki.csv', 'Uniwersytet_Śląski_w_Katowicach.csv']
+
+count_all = 0
+for uni in unis:
+    files = os.listdir(os.path.join(save_path, uni.replace('.csv', '')))
+    count_all += len(files)
 
 df_list = []
 for file in os.listdir(dir):
@@ -11,3 +23,12 @@ for file in os.listdir(dir):
 
 df = pd.concat(df_list)
 
+
+scihub = df[[True if 'sci' in str(link) else False for link in df.link.values]]
+
+
+# drop duplicates from journal
+df_unique = df.drop_duplicates(subset=['journal'])
+# reset index
+df_unique = df_unique.reset_index(drop=True)
+journal_counts = df_unique.journal.value_counts()
