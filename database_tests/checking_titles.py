505b122b3453048da24e52c0edc43105c73b93a2
Hubert Plisiecki <hplisiecki@gmail.com>
2023-08-30 14:31:18 +0200

    push

--- database_tests/checking_titles.py
+++ database_tests/checking_titles.py
@@ -6,7 +6,7 @@ import os
 failed_path = r'D:\data\ranking\pdfs\failed_pdf'
 save_path = r'D:\data\ranking\pdfs'
 links_path = r'D:\data\ranking\publication_links'
-new_links = r'D:\PycharmProjects\ranking\data\publications\orcid\export_to_automated'
+new_links_path = r'D:\PycharmProjects\ranking\data\publications\orcid\export_to_automated'
 
 unis = ['SWPS_Uniwersytet_Humanistycznospołeczny_z_siedzibą_w_Warszawie.csv', 'Akademia_Ignatianum_w_Krakowie.csv', 'Instytut_Psychologii_Polskiej_Akademii_Nauk.csv',
           'Akademia_Pedagogiki_Specjalnej_Marii_Grzegorzewskiej_w_Warszawie.csv', 'Uniwersytet_Adama_Mickiewicza_w_Poznaniu.csv', 'Katolicki_Uniwersytet_Lubelski_Jana_Pawła_II_w_Lublinie.csv',
@@ -17,15 +17,19 @@ unis = ['SWPS_Uniwersytet_Humanistycznospołeczny_z_siedzibą_w_Warszawie.csv', 
         'Akademia_Ekonomiczno-Humanistyczna_w_Warszawie.csv']
 
 for uni in unis:
-    # load links
+    print(uni)
     links = pd.read_csv(os.path.join(links_path, uni))
-    new_links = pd.read_csv(os.path.join(new_links, uni))
-    break
+    links = links[links['link'].str.contains('https', na = False)]
+    print("Length old links: ", len(links))
+    new_links = pd.read_csv(os.path.join(new_links_path, uni))
+    print("Length new links: ", len(new_links))
     new_links = new_links[['name', 'link', 'title', 'id']]
     # merge on link and name
-
-    links = links.merge(new_links, on=['name', 'link'], how='outer')
-    break
+    try:
+        links = links.merge(new_links, on=['name', 'link'], how='outer')
+        print("Length missing titles: ", len(links[links['title'].isna()]))
+    except:
+        print("Already updated")
 
 print(len(links[links['orcid'].isna()]))
 
